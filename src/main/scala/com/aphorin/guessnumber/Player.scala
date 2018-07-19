ec696723a1f64ab7088c87658ddd295d78e27c1b
aleksandr-lisenko <aleksandr.lisenko@accenture.com>
2018-07-19 23:06:52 +0300

    player actor

---
+++ src/main/scala/com/aphorin/guessnumber/Player.scala
@@ -0,0 +1,43 @@
+package com.aphorin.guessnumber
+
+import akka.actor.{Actor, ActorRef}
+
+object Player {
+
+  private[aphorin] final case class Connected(outgoing: ActorRef)
+
+  private[aphorin] final case class In(message: String)
+
+  private[aphorin] final case class Out(message: String)
+
+  private[aphorin] final class StringToEither(val s: String) {
+    def checked: Either[String, Int] = {
+      try {
+        Right(s.toInt)
+      } catch {
+        case _: Throwable => Left("Wrong format. Only integers accepted")
+      }
+    }
+  }
+
+  implicit def stringToEither(s: String) = new StringToEither(s)
+
+}
+
+final class Player(game: ActorRef) extends Actor {
+
+  import Player._
+
+  def receive = {
+    case Connected(outgoing) =>
+      context.become(connected(outgoing))
+      game ! Game.JoinGame
+  }
+
+  private def connected(outgoing: ActorRef): Receive = {
+    case In(text) => game ! Game.Guess(text.checked)
+
+    case msg: Out => outgoing ! msg
+  }
+
+}
